33b07f5a960b4cd573ad4bd77b5ec1a15bffdc82
Vihaga256 <vihagayasithawijewardhana29@gmail.com>
2026-05-05 03:10:46 +0530

    git commint

--- lib/views/new_loan_request_view/new_loan_request/new_client_loan_request.dart
+++ lib/views/new_loan_request_view/new_loan_request/new_client_loan_request.dart
@@ -207,70 +207,77 @@ class _NewClientLoanRequestState extends State<NewClientLoanRequest> {
                 SizedBox(height: _customSize_2),
                 customText("Loan Duration"),
                 SizedBox(height: _customSize_1),
-                DropdownButtonFormField<String>(
-                  hint: Text("Choose Loan Duration"),
-                  icon: const Icon(
-                    Icons.keyboard_arrow_down,
-                    color: Color(0xFF1A3D81),
-                  ),
-                  decoration: InputDecoration(
-                    floatingLabelStyle: TextStyle(fontSize: 1),
-                    errorMaxLines: 2,
-                    contentPadding: const EdgeInsets.symmetric(
-                      vertical: 0,
-                      horizontal: 12,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(btnBorderRadius),
+                Row(
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      child: DropdownButtonFormField<String>(
+                        hint: Text("Choose Loan Duration"),
+                        icon: const Icon(
+                          Icons.keyboard_arrow_down,
+                          color: Color(0xFF1A3D81),
+                        ),
+                        decoration: InputDecoration(
+                          floatingLabelStyle: TextStyle(fontSize: 1),
+                          errorMaxLines: 2,
+                          contentPadding: const EdgeInsets.symmetric(
+                            vertical: 0,
+                            horizontal: 12,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(btnBorderRadius),
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(btnBorderRadius),
+                            ),
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(58, 23, 23, 23),
+                              width: 1.5,
+                            ),
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(btnBorderRadius),
+                            ),
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(89, 181, 0, 0),
+                              width: 2,
+                            ),
+                          ),
+                          errorStyle: TextStyle(
+                            color: Color.fromARGB(255, 233, 1, 1),
+                            fontSize: 15,
+                            fontWeight: FontWeight(700),
+                          ),
+                          fillColor: safeAreaC,
+                          filled: true,
+                          labelText: "labelText_",
+                          labelStyle: TextStyle(
+                            color: Color.fromARGB(105, 21, 21, 21),
+                            fontSize: 17,
+                            fontWeight: FontWeight(500),
+                          ),
+                        ),
+                        items: ["6 Months", "12 Months", "24 Months"].map((
+                          String value,
+                        ) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(
+                              value,
+                              style: TextStyle(color: Color(0xFF1A3D81)),
+                            ),
+                          );
+                        }).toList(),
+                        onChanged: (newValue) {
+                          // Logic to update Monthly Payment based on duration
+                        },
                       ),
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(btnBorderRadius),
-                      ),
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(58, 23, 23, 23),
-                        width: 1.5,
-                      ),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(btnBorderRadius),
-                      ),
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(89, 181, 0, 0),
-                        width: 2,
-                      ),
-                    ),
-                    errorStyle: TextStyle(
-                      color: Color.fromARGB(255, 233, 1, 1),
-                      fontSize: 15,
-                      fontWeight: FontWeight(700),
-                    ),
-                    fillColor: safeAreaC,
-                    filled: true,
-                    labelText: "labelText_",
-                    labelStyle: TextStyle(
-                      color: Color.fromARGB(105, 21, 21, 21),
-                      fontSize: 17,
-                      fontWeight: FontWeight(500),
-                    ),
-                  ),
-                  items: ["6 Months", "12 Months", "24 Months"].map((
-                    String value,
-                  ) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(
-                        value,
-                        style: TextStyle(color: Color(0xFF1A3D81)),
-                      ),
-                    );
-                  }).toList(),
-                  onChanged: (newValue) {
-                    // Logic to update Monthly Payment based on duration
-                  },
+                  ],
                 ),
               ],
             ),
