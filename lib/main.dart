e768216bbe7ddc8f28edd2bb4730e8bdb020694e
shanuka02 <shanukagamlath@gmail.com>
2026-05-13 02:00:30 +0530

    fix conn

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nkrs_app/views/customer_collection_views/OnBording/onbordingScreen.dart';
-import 'package:nkrs_app/views/customer_collection_views/customerCollectionpage/collection_entry.dart' show CollectionEntryPage;
+import 'package:nkrs_app/views/customer_collection_views/customerCollectionpage/collection_entry.dart'
+    show CollectionEntryPage;
 import 'package:nkrs_app/views/customer_collection_views/loginpage/login_page.dart';
 
 void main() async {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       title: "NKRS App",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: "Inter"),
-      home: CollectionEntryPage(),
+      home: LoginPage(),
     );
   }
 }
