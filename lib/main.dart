13e0a4bd916ea0d68f90736ec23c8661e9fcbb52
shanuka02 <shanukagamlath@gmail.com>
2026-05-14 21:26:24 +0530

    Revert "fix conn"
    This reverts commit e768216bbe7ddc8f28edd2bb4730e8bdb020694e.
    
    wq
    :wq

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nkrs_app/views/customer_collection_views/OnBording/onbordingScreen.dart';
-import 'package:nkrs_app/views/customer_collection_views/customerCollectionpage/collection_entry.dart'
-    show CollectionEntryPage;
+import 'package:nkrs_app/views/customer_collection_views/customerCollectionpage/collection_entry.dart' show CollectionEntryPage;
 import 'package:nkrs_app/views/customer_collection_views/loginpage/login_page.dart';
 
 void main() async {
@@ -18,7 +17,7 @@ class MyApp extends StatelessWidget {
       title: "NKRS App",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: "Inter"),
-      home: LoginPage(),
+      home: CollectionEntryPage(),
     );
   }
 }
