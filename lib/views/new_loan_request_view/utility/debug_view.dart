4fdfa7f57956334e8a0f7422d1c69b9e105ccb6b
Vihaga256 <vihagayasithawijewardhana29@gmail.com>
2026-06-09 00:24:32 +0530

    update

--- lib/views/new_loan_request_view/utility/debug_view.dart
+++ lib/views/new_loan_request_view/utility/debug_view.dart
@@ -6,8 +6,7 @@ import 'package:nkrs_app/data/view_model/async_database_table.dart';
 import 'package:nkrs_app/data/view_model/check_connection.dart';
 import 'package:nkrs_app/utility/constanst.dart';
 import 'package:nkrs_app/views/new_loan_request_view/utility/popup_box_message.dart';
-import 'package:nkrs_app/views/sync_view/connection_view.dart'
-    hide CheckConnection;
+import 'package:nkrs_app/views/sync_view/connection_view.dart';
 import 'package:nkrs_app/views/sync_view/sync_async_view.dart';
 
 class DebugView extends StatefulWidget {
